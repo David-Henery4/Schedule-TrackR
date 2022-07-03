7909c0a110c87d3e63a54ca6db21b8c0f8d7bd0e
DizzlyD <david4henery00@hotmail.com>
2022-07-04 00:13:47 +0100

    calandar-data

--- src/pages/weeks.js
+++ src/pages/weeks.js
@@ -1,6 +1,12 @@
 import React from 'react'
 import Calandar from '../components/calandar'
 
+/**
+ * Get the amount of days in a month (Based on the year & month)
+ * @param {Number} Y Year
+ * @param {Number} m Month
+ * @returns Amount of days in a month
+ */
 const monthDays = (y,m) => {
   return new Date(y,m, 0).getDate()
 }
@@ -10,9 +16,40 @@ const thisYear = todayDate.getFullYear()
 const thisMonth = todayDate.getMonth() + 1
 //
 const currentMonthDays = monthDays(thisYear,thisMonth)
-console.log(currentMonthDays)
-console.log(monthDays(2023,2)) // feb next year
+/**
+ * Creates an array from the amount of days from the 'monthDays function
+ */
+const currentAmountOfDays = []
+for(let i = 0; i < currentMonthDays; i++){
+  currentAmountOfDays.push(i+1)
+}
 //
+const calandarData = {
+  daysOfMonth: {
+    date: [],
+    name:[],
+  },
+  year: 0,
+  month: ""
+}
+//
+todayDate.setMonth(thisMonth - 1)
+const monthString = todayDate.toLocaleString("default", { month: "long"});
+calandarData.month = monthString
+calandarData.year = thisYear
+//
+currentAmountOfDays.forEach((d,_,a) => {
+  const dates = new Date(thisYear, thisMonth - 1, d);
+  calandarData.daysOfMonth.date.push(d);
+  const dayNames = dates.toLocaleDateString("default", { weekday: "short" });
+  calandarData.daysOfMonth.name.push(dayNames);
+  // might need to come back.
+  // calandarData.daysOfMonth[dayNames] = [d]
+})
+
+
+
+console.log(calandarData)
 
 
 export const Weeks = () => {
